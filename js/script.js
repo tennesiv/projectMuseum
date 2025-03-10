65391a7216f9066b5b41b8d824804b12401204f3
Michil <godof830@gmail.com>
2025-03-10 21:24:02 +0900

    sc

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// кнопка "наверх"
+document.addEventListener("DOMContentloaded", () => {
+	const btn = document.createElement('button');
+	btn.innerText = "Наверх";
+	btn.style.position = "fixed";
+	btn.style.botton = "20px";
+	btn.style.right = "20px";
+	btn.style.padding = "10px 20px";
+	btn.style.borderRadius = "8px";
+	btn.style.boder = "none";
+	btn.style.backgroundColor = "#f39c12";
+	btn.style.color = "fff";
+	btn.style.cursor = "pointer";
+	btn.style.boxShadow = "0 4px 8px rgba(0,0,0,0.2)";
+	btn.style.display = "none";
+});
+	
